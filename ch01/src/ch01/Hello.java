368e95c348221e425950fd20485a7cfbc8a148d6
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-01-03 15:34:25 +0900

    ch01 add

---
+++ ch01/src/ch01/Hello.java
@@ -0,0 +1,15 @@
+package ch01;
+
+/*
+ * 날짜 : 2024/12/31
+ * 이름 : 정진우
+ * 내용 : ch01.java 개발환경 구축
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ ch01/src/ch01/PrintTest.java
@@ -0,0 +1,26 @@
+package ch01;
+
+/*
+ * 날짜 : 2024/12/31
+ * 이름 : 정진우
+ * 내용 : 출력 테스트	
+ */
+
+public class PrintTest {
+	public static void main(String[] args) {
+		//기본 출력
+		System.out.println("Hello Java!");
+		System.out.println("Hello Korea!");
+		System.out.println("Hello Busan!");
+		
+		//한 줄 출력
+		System.out.print("Welcome "); // ln 줄바꿈(개행) 처리가 안 되기 때문에 출력 문자열이 이어서 출력됨.
+		System.out.print("Java, ");
+		System.out.print("Spring!");
+		
+		//개행 출력
+		System.out.println(); // 줄바꿈(개행)
+		System.out.print("Greeting\n"); // \n 이스케이프 문자로 줄바꿈(개행) 처리
+		System.out.print("Java");
+	}
+}
