669acbe834f76c4bb0dcc744c5bd174897ecd834
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-01-07 17:43:32 +0900

    updated...

--- CodingTest/src/step2/Test06.java
+++ CodingTest/src/step2/Test06.java
@@ -1,5 +1,7 @@
 package step2;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2025.01.07
  * 이름 : 곽혜수
@@ -7,5 +9,35 @@ package step2;
  */
 
 public class Test06 {
-
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int t = sc.nextInt();
+		
+		if((m+t)>=60) {
+			h++;
+			m = (m+t)-60;
+			
+			while(m>=60) {
+				h++;
+				m = m-60;
+			}
+			
+			if(h>23) {
+				h = 0+(24-h);
+			}
+			
+			System.out.println(h + " " + m);
+			
+		}else {
+			m = m + t;
+			System.out.println(h + " " + m);
+			
+		}
+		
+		sc.close();
+	}
 }
--- CodingTest/src/step2/Test07.java
+++ CodingTest/src/step2/Test07.java
@@ -1,5 +1,7 @@
 package step2;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2025.01.07
  * 이름 : 곽혜수
@@ -7,5 +9,52 @@ package step2;
  */
 
 public class Test07 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = sc.nextInt();
+		
+		if(x==y && y==z) {
+			
+			int prize = 10000 + (y * 1000);
+			System.out.println(prize);
+			
+		}else if(x==z){
+			int prize = 1000 + (x * 100);
+			System.out.println(prize);
+			
+		}else if(x==y || y==z) {
+			
+			int prize = 1000 + (y * 100);
+			System.out.println(prize);
+			
+		}else{
+			
+			if(x>y && x>z) {
+				int prize = 100 * x ;
+				System.out.println(prize);
+				
+			}else if(x<y && y>z) {
+				int prize = 100 * y ;
+				System.out.println(prize);
+				
+			}else if(x<y && y<z) {
+				int prize = 100 * z ;
+				System.out.println(prize);
+				
+			}else if(x>y && x<z) {
+				int prize = 100 * z ;
+				System.out.println(prize);
+				
+			}
+			
+		}
+		
+		sc.close();
+	}
 
 }
