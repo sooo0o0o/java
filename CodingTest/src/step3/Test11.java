bfc69de70c1657e9304be5939290e6c4ec89c470
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-01-14 13:59:46 +0900

    updated step3

--- CodingTest/src/step3/Test11.java
+++ CodingTest/src/step3/Test11.java
@@ -1,11 +1,42 @@
 package step3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 /*
  * 날짜 : 2025.01.08
  * 이름 : 곽혜수
- * 내용 : 백준 3단계 11번 
+ * 내용 : 백준 3단계 11번 A+B (5)
  */
 
-public class Test11 {
+//0 0 을 입력하면 식이 종료
 
+public class Test11 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		while(true) {
+			
+			String[] input = bf.readLine().split(" ");
+			
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+								
+			if(x == 0 && y == 0 ) break;
+			
+			bw.write((x+y) + "\n");
+			
+			
+			
+		}
+				
+		bw.flush();
+		bw.close();
+		
+	}
 }
--- CodingTest/src/step3/Test12.java
+++ CodingTest/src/step3/Test12.java
@@ -1,11 +1,33 @@
 package step3;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2025.01.08
  * 이름 : 곽혜수
- * 내용 : 백준 3단계 12번 
+ * 내용 : 백준 3단계 12번 A+B (4)
  */
 
 public class Test12 {
-
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(sc.hasNextInt()) {
+			
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+		
+			System.out.println(x+y);			
+			
+		}
+		
+		
+		sc.close();
+		
+	}
+	
+	
 }
