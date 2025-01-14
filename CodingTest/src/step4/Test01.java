02241a39c2fe9098ad2edb1a62d173d85d465e9e
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-01-14 17:36:35 +0900

    updated step4 test01,02

--- CodingTest/src/step4/Test01.java
+++ CodingTest/src/step4/Test01.java
@@ -1,11 +1,38 @@
 package step4;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2025.01.08
  * 이름 : 곽혜수
- * 내용 : 백준 4단계 1번
+ * 내용 : 백준 4단계 1번 개수세기
  */
 
 public class Test01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		int num[] = new int[t];		// num[] 배열 생성
+		
+		
+		for(int i=0; i < t; i++) {
+			num[i] = sc.nextInt();	// num[i] = 입력받은 숫자, until i < t
+					
+		}
+		
+		int x = sc.nextInt();	// 찾을 숫자 x
+		
+		int sum = 0;
+		for(int list : num) {
+			
+			if ( list == x ) sum++;	//list 와 x 를 비교해 갯수를 ++
+		
+		}
+		
+		System.out.println(sum);
+		sc.close();
+	}
 
 }
--- CodingTest/src/step4/Test02.java
+++ CodingTest/src/step4/Test02.java
@@ -1,11 +1,42 @@
 package step4;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2025.01.08
  * 이름 : 곽혜수
- * 내용 : 백준 4단계 1번
+ * 내용 : 백준 4단계 2번 x 보다 작은수
  */
 
 public class Test02 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int numArr[] = new int[n];
+		
+		int compare = sc.nextInt();
+		
+		for(int i = 0 ; i < n ; i ++) {
+			
+			numArr[i] = sc.nextInt();
+			
+		}
+		
+		int num = 0;
+		
+		for(int list : numArr) {
+			
+			if(list < compare) {
+				num = list;
+				System.out.print(num+ " ");
+			}
+		}
+		
+		
+		sc.close();
+		
+	}
 
 }
